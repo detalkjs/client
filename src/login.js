52f17f49a51015b0c91c645e83fac3bb32a2bead
CKY <awa@outlook.ie>
2022-12-28 12:08:11 +0800

    :sparkles: 支持 GitHub 登录

--- src/login.js
+++ src/login.js
@@ -9,8 +9,10 @@ export default async function login() {
     _id_s("detalk-warn").innerText = "请在登录窗口中继续";
     // }
     // _id_s("_detalk_detail").appendChild(ele);
+    let github = "";
+    if (DETALK_INIT.login.github) github = "&github=" + DETALK_INIT.login.github;
     window.open(
-        "https://detalk-dash.netlify.app/login.html?url=" + encodeURIComponent(DETALK_INIT.url) + "&framelogin=true",
+        "https://detalk-dash.netlify.app/login.html?url=" + encodeURIComponent(DETALK_INIT.url) + "&framelogin=true" + github,
         '_blank',
         `width=600,height=600,scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no`
     );
@@ -23,7 +25,7 @@ export default async function login() {
             console.log(data);
             if (data.admin) localStorage.setItem("DETALK_IS_ADMIN", true);
             localStorage.setItem("DETALK_AUTH", token);
-            fetch(DETALK_INIT.url + "/_api/profile?token="+token, {
+            fetch(DETALK_INIT.url + "/_api/profile?token="+token+github, {
                 method: "GET",
             }).then(res => res.json()).then(res => {
                 if (res.success) {
