088b3c838bd3fd7a66fabf3ff9c7b0fa3c23b0ee
ChunKam Yu <ohhhcky@outlook.com>
2023-04-04 16:13:17 +0800

    :bug: 修复 reCAPTCHA 反复加载的错误

--- src/lib/recaptcha.js
+++ src/lib/recaptcha.js
@@ -9,7 +9,7 @@ export default async function recaptcha(siteKey, reload) {
 
 
     // 载入 reCAPTCHA
-    if (window.DETALK_INIT && !window.DETALK_INIT.recaptchaLoaded) {
+    if (window.DETALK_INIT && !window.DETALK_INIT.recaptchaLoaded && !window.grecaptcha) {
         let script = document.createElement('script');
         script.src = 'https://www.recaptcha.net/recaptcha/api.js?render=' + siteKey || window.DETALK_RECAPTCHA_SITE_KEY;
         script.async = true;
@@ -18,9 +18,17 @@ export default async function recaptcha(siteKey, reload) {
             grecaptcha.ready(function() {
                 grecaptcha.execute(siteKey || window.DETALK_RECAPTCHA_SITE_KEY, {action: 'DetalkJS'}).then(function(token) {
                     window.DETALK_RECAPTCHA_TOKEN = token;
+                    window.DETALK_INIT.recaptchaLoaded = true;
                 });
             });
         }
         document.body.appendChild(script);
+    } else if (window.DETALK_INIT.recaptchaLoaded || window.grecaptcha) {
+        // 有 reCAPTCHA script, 直接执行
+        grecaptcha.ready(function() {
+            grecaptcha.execute(siteKey || window.DETALK_RECAPTCHA_SITE_KEY, {action: 'DetalkJS'}).then(function(token) {
+                window.DETALK_RECAPTCHA_TOKEN = token;
+            });
+        });
     }
 }
